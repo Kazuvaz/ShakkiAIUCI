a760f9493c414f4dbb0267fa982b23bf38283b0f
Kazuvaz <kazuvaza@outlook.com>
2025-04-19 21:39:09 +0300

    poytry

--- Testit.py
+++ Testit.py
@@ -2,50 +2,47 @@ import ShakkiAIUCI
 import random
 import MinMaxTree
 import Evaluator
+import unittest
+
 #pelaa satunnaisia siirtoja n kertaa tulostaa siirrot. tulostaa myös laudan tilan alussa ja lopussa
-def randomMoves(n : int, lauta : ShakkiAIUCI.board):
-    print()
-    lautab : ShakkiAIUCI.board= lauta
-    lautab.prints()
-    for i in range(0,n):
-        con : list=  lautab.trimmedContinuations()
-        #jos kuningas puuttuu pöydältä 
-        test = False
-        for l in range(0,8):
-            if 'k' in lautab.pieces[l]:
-                test = True
-        if not test:
-            print("what?")
-            break
-                
+class TestShakkiAIUCI(unittest.TestCase):
+    opening : ShakkiAIUCI.board
+    def setUp(self):
+        #alkutilanne
+        pieces =[['0' for i in range(8)] for j in range(8)]
+        pieces[0] = ["r","n","b","q","k","b","n","r"]
+        pieces[1] = ["p","p","p","p","p","p","p","p"]
+        pieces[6] = ["P","P","P","P","P","P","P","P"]
+        pieces[7] = ["R","N","B","Q","K","B","N","R"]
+        self.opening =  ShakkiAIUCI.board(pieces,True,[True,True,True,True])
+        return
 
-        if len(con) == 0:
-            print("mate")
-            break 
-        lautab = con[random.randrange(0,len(con))]
-        print(lautab.prev )
-    lautab.prints()
+    def test_loytaa_kaikki_siirrot_aloituksessa(self):
+        maara = len(self.opening.legalContinuationsUntrimmed())
+        self.assertEqual(maara, 20)
 
-#kaikki mahdolliset siirrot tilanteesta lauta
-def allMoves(lauta : ShakkiAIUCI.board):
-    con : list=  lauta.trimmedContinuations()
-    for a in con:
-        p : ShakkiAIUCI.board = a
-        print(p.prev)
-
-def evaluate(n :int,lauta : ShakkiAIUCI.board):
-    start : MinMaxTree.Node = MinMaxTree.Node(lauta)
-    for i in range(0,n):
-        start.sprout()
-    for i in start.children:
-        nod : MinMaxTree.Node = i
-        print(nod.board.prev + " " + str( nod.value))
-    #start.bestMove()
-    #start.bestLine()
+    def test_vuoro_vaihtuu(self):
+        self.assertEqual(self.opening.turn,True)
+        self.opening.move('a1a1')
+        self.assertEqual(self.opening.turn,False)
+    def test_linnoitus_loytyy(self):
+        self.opening.move('f1f3')
+        self.opening.move('g1g3')
+        he : bool = 'e1g1' in self.opening.legalContinuationsUntrimmed()
+        self.assertEqual(he,True)
+    def test_MoukkaSyo(self):
+        self.opening.move('e2e4')
+        self.opening.move('d7d5')
+        he : bool = 'e4d5' in self.opening.legalContinuationsUntrimmed()
+        self.assertEqual(he,True)
 
 
 
+tes = TestShakkiAIUCI()
+tes.setUp()
+tes.test_MoukkaSyo
 
+'''
 #alkutilanne
 pieces =[['0' for i in range(8)] for j in range(8)]
 pieces[0] = ["r","n","b","q","k","b","n","r"]
@@ -91,7 +88,7 @@ opening.move('e1f1')
 
 evaluate(300,opening)
 
-'''
+
 
 
 customPosition.move("g2h3")
---
+++ documentaatio/Viikko6.md
@@ -0,0 +1,2 @@
+# Viikko 5
+poytry implementoitu ja UCI implementoitu toimimaan ainakin tarpeeksi hyvin että AIta vastaan voi pelata chessarena ja encroissan shakki GUI avulla
